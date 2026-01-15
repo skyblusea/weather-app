672694f867842dbb7fae741f3c70e8082f4524f6
skyblusea <skyyblusea@gmail.com>
2026-01-15 19:41:40 +0900

    feat: 상세 페이지 라우터 추가

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,10 +1,12 @@
-import { Routes, Route } from "react-router";
+import { DetailPage } from "@/pages/detail";
 import { HomePage } from "@/pages/home";
+import { Route, Routes } from "react-router";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/detail" element={<DetailPage />} />
     </Routes>
   );
 };
---
+++ src/pages/detail/ui/DetailPage.tsx
@@ -0,0 +1,3 @@
+export function DetailPage() {
+  return <div className="mx-auto my-8 flex min-h-screen w-full max-w-lg flex-col gap-4 overflow-y-auto px-4"></div>;
+}
