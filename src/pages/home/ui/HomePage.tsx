c4f9ee7cf83635dd786372b074364f3ad4f40af1
skyblusea <skyyblusea@gmail.com>
2026-01-15 15:57:32 +0900

    feat: 현재 위치 기반 날씨 정보 표시 기능 추가

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,37 +1,22 @@
-import type { WeatherCondition } from "@/entities/weather/model/types";
+import type { Location } from "@/entities/location/model/types";
+import { useCurrentLocation } from "@/entities/location/model/useCurrentLocation";
 import { SearchBar } from "@/widget/DistrictSearchBar";
 import { FavoritesGrid } from "@/widget/FavoritesGrid";
 import { WeatherPanel } from "@/widget/WeatherPanel";
-import type { HourlyWeather } from "@/widget/WeatherPanel/types";
+import { useState } from "react";
 
 export function HomePage() {
-  const currentWeatherMock = {
-    currentTemp: 1.4,
-    todayMinTemp: 3,
-    todayMaxTemp: 8,
-    condition: "SUNNY" as WeatherCondition,
-  };
+  const { currentLocation } = useCurrentLocation();
 
-  const currentLocationMock = {
-    path: "서울특별시 광진구 광장동",
-    code: "1121581000",
-    gridX: 60,
-    gridY: 126,
-  };
-
-  const hourlyWeatherMock = new Map<string, HourlyWeather>();
-  for (let h = 0; h < 24; h++) {
-    hourlyWeatherMock.set(h.toString(), {
-      time: h.toString(),
-      condition: "SUNNY",
-      currentTemp: 0,
-      source: "MISSING",
-    });
-  }
+  const [selectedLocation, setSelectedLocation] = useState<Location>({
+    nx: currentLocation.nx,
+    ny: currentLocation.ny,
+    name: currentLocation.name,
+  });
 
   return (
     <div className="mx-auto my-8 flex min-h-screen w-full max-w-lg flex-col gap-4 overflow-y-auto px-4">
-      <WeatherPanel {...currentWeatherMock} locationName={currentLocationMock.path} hourlyWeather={hourlyWeatherMock} />
+      <WeatherPanel {...selectedLocation} />
       <SearchBar />
       <FavoritesGrid favorites={[]} />
     </div>
