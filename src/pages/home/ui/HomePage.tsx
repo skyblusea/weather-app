2bddb2ba5fa46964160441746350e48c7cd7efc9
skyblusea <skyyblusea@gmail.com>
2026-01-15 19:35:18 +0900

    refactor: 날씨 데이터 컴포넌트 단에서 fetching

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -16,7 +16,7 @@ export function HomePage() {
     <div className="mx-auto my-8 flex min-h-screen w-full max-w-lg flex-col gap-4 overflow-y-auto px-4">
       <WeatherPanel {...selectedLocation} />
       <DistrictSearchBar onLocationSelect={setSelectedLocation} />
-      <FavoritesGrid favorites={[]} />
+      <FavoritesGrid />
     </div>
   );
 }
